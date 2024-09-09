90be91697001672ab871f9432a9fcb3eddb1f372
yesonnim <lena.son01@gmail.com>
2024-09-09 23:53:02 +0900

    Initial Commit

---
+++ fg/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  fg
+//
+//  Created by Yelena Son on 09.09.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
